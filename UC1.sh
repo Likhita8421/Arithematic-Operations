c172e325c17c0e5171fee729151a261e5c5632de
HP <leenupandagre24@gmail.com>
2022-04-02 15:50:04 +0530

    added implementation

--- UC1.sh
+++ UC1.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -x
 
 echo "enter number1"
-read number1
+read a
 
 echo "enter number2"
-read number2
+read b
 
 echo "enter number3"
-read number3
+read c
+
+result=$(("$a + $b * $c"));
+echo "result : $result"
